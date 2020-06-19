d6a5c02ce079e6643edf52f00eee173b42b5561f
Joshua Francis <jmahendr@users.noreply.github.com>
2020-06-20 09:46:00 +1000

    Use Lambda proxy and expose as API via apigateway

--- aws/handlers/find_user.py
+++ aws/handlers/find_user.py
@@ -10,24 +10,35 @@ import boto3
 logger = logging.getLogger(__name__)
 logger.setLevel(getattr(logging, os.environ['logLevel'], 40))
 
+# Setup CORS
+cors_domain = os.environ['CORS_DOMAIN']
+cors = {
+    'Content-Type': 'application/json',
+    'Access-Control-Allow-Origin': cors_domain
+}
 
 def lambda_handler(event, context):
     logger.info(f"Received {event=}")
     user_pool_id = os.environ['userPoolId']
     attributes_string = os.environ['attributes']
-    email = event['email']
+    email = event['queryStringParameters']['email']
 
     attributes = attributes_string.split(',')
     filter = f"email=\'{email}\'"
 
     client = boto3.client("cognito-idp")
-    response = client.list_users(
+    lis_user_resp = client.list_users(
         UserPoolId=user_pool_id,
         AttributesToGet=attributes,
         Limit=1,
         Filter=filter
     )
-    return json.dumps(response['Users'], default=datetime_handler)
+    response = {
+        "statusCode": 200,
+        "headers": cors,
+        "body": json.dumps(lis_user_resp['Users'], default=datetime_handler)
+    }
+    return response
 
 
 def datetime_handler(x):
